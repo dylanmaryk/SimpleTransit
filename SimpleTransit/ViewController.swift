c0e2b32277fa4775487a9e0b77709b0b77655fd7
Dylan Maryk <dylan@dylanmaryk.com>
2016-03-13 17:12:27 +0000

    Initial Commit

---
+++ SimpleTransit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SimpleTransit
+//
+//  Created by Dylan Maryk on 13/03/2016.
+//  Copyright © 2016 Dylan Maryk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
