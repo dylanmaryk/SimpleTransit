cdc19a374a1aa1207cec414e74c43c81b3d4f491
Dylan Maryk <dylan@dylanmaryk.com>
2016-03-14 01:04:24 +0000

    Transform lat and lng to CLLocation.

--- SimpleTransit/DataClasses/Stop.swift
+++ SimpleTransit/DataClasses/Stop.swift
@@ -6,23 +6,26 @@
 //  Copyright © 2016 Dylan Maryk. All rights reserved.
 //
 
+import CoreLocation
 import Foundation
 
 class Stop {
-    var lat: Double?
-    var lng: Double?
+    var location: CLLocation?
     var dateTime: NSDate?
     var name: String?
     
     init(lat: Double?, lng: Double?, dateTime dateTimeString: String?, name: String?) {
-        self.lat = lat
-        self.lng = lng
         self.name = name
+        
+        if let latToConvert = lat,
+            let lngToConvert = lng {
+                location = CLLocation(latitude: latToConvert, longitude: lngToConvert)
+        }
         
         if let dateTimeStringToConvert = dateTimeString {
             let dateFormatter = NSDateFormatter()
             dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZ"
-            self.dateTime = dateFormatter.dateFromString(dateTimeStringToConvert) // Date differs from original data according to timezone
+            dateTime = dateFormatter.dateFromString(dateTimeStringToConvert) // Date differs from original data according to timezone
         }
     }
 }
